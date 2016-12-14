583be2e38928e74c95ade4e4773a218c7ec1132d
dgormez <dgormez@gmail.com>
2016-12-14 13:29:27 +0100

    First commit - start of the project

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DGO.StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        public int Add(string numbers)
+        {
+            return new int();
+        }
+    }
+}
