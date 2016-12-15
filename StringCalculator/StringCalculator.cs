37b8bad7dd8025934ccbb35db90bf40473f8f34a
dgormez <dgormez@gmail.com>
2016-12-15 13:42:35 +0100

    Detects negative numbers and throw exception

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -8,6 +8,7 @@ namespace DGO.StringCalculatorKata
 {
     public class StringCalculator
     {
+        private char[] _supportedDelimiters = {',','\n' }; 
         public void Dispose()
         {
             GC.SuppressFinalize(this);
@@ -15,9 +16,55 @@ namespace DGO.StringCalculatorKata
 
         public int Add(string numbers)
         {
+            int result = 0;
+            if (IsHeaderPresent(numbers))
+            {
+                UpdateDelimiters(numbers);
+                numbers = RemoveHeader(numbers);
+            }
+
             if (string.IsNullOrEmpty(numbers)) {return 0;}
 
-            return int.Parse(numbers);
+            string[] splittedNumbers = SplitString(numbers);
+            int[] numbersToSum = Array.ConvertAll(splittedNumbers, int.Parse);
+
+            int[] negativeNumbersFromArray = numbersToSum.Where(x => x < 0).ToArray();
+            if (negativeNumbersFromArray.Length != 0)
+                throw new Exception(string.Format("Negatives not allowed: {0}",negativeNumbersFromArray.ToString()));
+
+
+            for (int i = 0; i < numbersToSum.Length; i++)
+            {
+                result += numbersToSum[i]; 
+            }
+
+            return result;
         }
+
+        public string[] SplitString(string stringToSplit)
+        {
+            return stringToSplit.Split(_supportedDelimiters);
+        }
+
+        public bool IsHeaderPresent(string numbers)
+        {
+            if (numbers.StartsWith("//"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void UpdateDelimiters(string numbers) 
+        {
+            _supportedDelimiters = new char[1];
+            _supportedDelimiters[0] = numbers[2];
+        }
+
+        public string RemoveHeader(string numbers)
+        {
+            return numbers.Remove(0, 4);
+        }
+
     }
 }
---
+++ TestsStringCalculator/StringCalculatorTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DGO.StringCalculatorKata;
+
+namespace DGO.TestsStringCalculatorKata
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        StringCalculator StringCalculator;
+
+        #region InitializeAndClean
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            StringCalculator = new StringCalculator();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            StringCalculator.Dispose();
+        }
+        #endregion
+
+        #region Individual Unit Tests
+        [TestMethod]
+        public void Test_Add_empty_zero()
+        {
+            int expected = 0;
+            string input = string.Empty;
+
+            int actual = StringCalculator.Add(input);
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Test_Add_SingleNumber_NumberAsInt()
+        {
+            int[] expected = {1,2,15};
+            string[] input = {"1","2","15"};
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int actual = StringCalculator.Add(input[i]);
+                Assert.AreEqual( expected[i],actual);
+            }
+        }
+        
+        [TestMethod]
+        public void Test_Add_TwoCommaSeparatedNumbers_SumOfTheTwoNumbers()
+        {
+            int[] expected = { 2, 2, 21 };
+            string[] input = { "1,1", "2", "15,6" };
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int actual = StringCalculator.Add(input[i]);
+                Assert.AreEqual(expected[i], actual);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Add_MultpileCommaSeparatedNumbers_SumOfTheNumbers()
+        {
+            int[] expected = { 9};
+            string[] input = { "1,1,1,2,4"};
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int actual = StringCalculator.Add(input[i]);
+                Assert.AreEqual(expected[i], actual);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Add_MultpileNumberSpecialDelimiters_SumOfTheNumbers()
+        {
+            int[] expected = { 6};
+            string[] input = { "1\n2,3"};
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int actual = StringCalculator.Add(input[i]);
+                Assert.AreEqual(expected[i], actual);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Add_HeaderDelimiter_SumOfTheNumbers()
+        {
+            int[] expected = { 3 };
+            string[] input = { "//;\n1;2" };
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int actual = StringCalculator.Add(input[i]);
+                Assert.AreEqual(expected[i], actual);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        //[ExpectedException(typeof(ArgumentException), "A userId of null was inappropriately allowed.")]
+        public void Test_Add_NegativeNumber_Exception() 
+        {
+            string input = "//;\n1;2;-5;2;-8";
+            StringCalculator.Add(input);
+        } 
+        #endregion
+        
+    }
+}
