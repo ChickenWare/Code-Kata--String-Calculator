fe1a0757f737376daac5bcf078faff401b932ae6
dgormez <dgormez@gmail.com>
2016-12-15 18:56:21 +0100

    Working step 6 - Before refactoring to use strings as delimiters

--- TestsStringCalculator/StringCalculatorTests.cs
+++ TestsStringCalculator/StringCalculatorTests.cs
@@ -117,13 +117,31 @@ namespace DGO.TestsStringCalculatorKata
             Assert.AreEqual(expected, StringCalculator.Add(input));  
         } 
 
-        [TestMethod]
-        public void Test_Add_MultipleCharDelimiter_SumOfNumbers()
-        {
-            string input = "//[***]\n1***2***3";
-            int expected = 6;
-            Assert.AreEqual(expected, StringCalculator.Add(input));  
-        } 
+        //[TestMethod]
+        //public void Test_Add_MultipleCharDelimiter_SumOfNumbers()
+        //{
+        //    string input = "//[***]\n1***2***3";
+        //    int expected = 6;
+        //    Assert.AreEqual(expected, StringCalculator.Add(input));  
+        //}
+
+        //[TestMethod]
+        //public void Test_Add_SpecialCaseMultipleCharDelimiter_SumOfNumbers()
+        //{
+        //    string input = "//[]]]]\n1]]]2]]]3";
+        //    int expected = 6;
+        //    Assert.AreEqual(expected, StringCalculator.Add(input));
+        //} 
+
+        //[TestMethod]
+        //public void Test_Add_SpecialCaseSingleDelimiter_SumOfNumbers()
+        //{
+        //    string input = "//[\n1[2[3";
+        //    int expected = 6;
+        //    Assert.AreEqual(expected, StringCalculator.Add(input));
+        //}
+
+
         #endregion
         
     }
